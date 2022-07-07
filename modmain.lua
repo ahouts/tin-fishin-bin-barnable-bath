3524c0f14bc961bfba823bd352e9c720c3a01288
Andrew Houts <ahouts4@gmail.com>
2022-07-07 11:53:59 -0500

    improve safety for future api changes

--- modmain.lua
+++ modmain.lua
@@ -2,10 +2,23 @@ AddPrefabPostInitAny(function(inst)
     if not inst or inst.prefab ~= "fish_box" then
         return
     end
-    local old_fn = GLOBAL.rawget(inst.components.container, "_").itemtestfn[1]
-    local new_fn = function(container, item, slot)
-        return old_fn(container, item, slot) or (item and item.prefab == "barnacle")
+    local util = require("dst-util/util")(GLOBAL)
+
+    local old_fn = util.access(inst, "components", "container", util.get_class, "itemtestfn", 1)
+    if old_fn == nil then
+        util.error("implementation of Tin Fishin' Bin changed, contact mod author!")
+        return
     end
-    GLOBAL.rawget(inst.components.container, "_").itemtestfn[1] = new_fn
-    GLOBAL.rawget(inst.replica, "_").container.itemtestfn = new_fn
+    local new_fn = function(container, item, slot)
+        return old_fn(container, item, slot) or (util.access(item, "prefab") == "barnacle")
+    end
+
+    util.access(inst, "components", "container", util.get_class, "itemtestfn")[1] = new_fn
+
+    local container_replica = util.access(inst, "replica", util.get_class, "container")
+    if container_replica == nil then
+        util.error("implementation of Tin Fishin' Bin replica changed, contact mod author!")
+        return
+    end
+    container_replica.itemtestfn = new_fn
 end)
