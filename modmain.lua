4893168d297060f49bc555ccb6c1977126478598
Andrew Houts <ahouts4@gmail.com>
2022-07-07 18:03:18 -0500

    avoid partial mod application

--- modmain.lua
+++ modmain.lua
@@ -4,8 +4,10 @@ AddPrefabPostInitAny(function(inst)
     end
     local util = require("dst-util/util")(GLOBAL)
 
-    local old_fn = util.access(inst, "components", "container", util.get_class, "itemtestfn", 1)
-    if old_fn == nil then
+    local old_test_fn = util.access(inst, "components", "container", util.get_class, "itemtestfn")
+    local old_fn = util.access(old_test_fn, 1)
+    local container_replica = util.access(inst, "replica", util.get_class, "container")
+    if util.any_nil(old_fn, container_replica) then
         util.error("implementation of Tin Fishin' Bin changed, contact mod author!")
         return
     end
@@ -13,12 +15,6 @@ AddPrefabPostInitAny(function(inst)
         return old_fn(container, item, slot) or (util.access(item, "prefab") == "barnacle")
     end
 
-    util.access(inst, "components", "container", util.get_class, "itemtestfn")[1] = new_fn
-
-    local container_replica = util.access(inst, "replica", util.get_class, "container")
-    if container_replica == nil then
-        util.error("implementation of Tin Fishin' Bin replica changed, contact mod author!")
-        return
-    end
+    old_test_fn[1] = new_fn
     container_replica.itemtestfn = new_fn
 end)
