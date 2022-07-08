fd8e74f1d5b6919848a4b00a5dc511d7c2f1d8ad
Andrew Houts <ahouts4@gmail.com>
2022-07-08 18:54:24 -0500

    use overengineered type checker instead of a few if statements

--- modmain.lua
+++ modmain.lua
@@ -3,18 +3,34 @@ AddPrefabPostInitAny(function(inst)
         return
     end
     local util = require("dst-util/util")(GLOBAL)
+    local type = require("dst-util/type")(util)
 
-    local old_test_fn = util.access(inst, "components", "container", util.get_class, "itemtestfn")
-    local old_fn = util.access(old_test_fn, 1)
-    local container_replica = util.access(inst, "replica", util.get_class, "container")
-    if util.any_nil(old_fn, container_replica) then
+    local container_instance_type = type.Table({
+        components = type.Table({
+            container = type.Class("Container", {
+                itemtestfn = type.Table({
+                    type.Function(),
+                }),
+            }),
+        }),
+        replica = type.Class("Replica", {
+            container = type.Table({
+                itemtestfn = type.Function(),
+            }),
+        }),
+    })
+
+    if not type.type_check(container_instance_type, inst) then
+        util.display(container_instance_type.describe())
+        util.display(inst)
         util.error("implementation of Tin Fishin' Bin changed, contact mod author!")
         return
     end
+    local old_fn = util.get_class(inst.components.container).itemtestfn[1]
     local new_fn = function(container, item, slot)
         return old_fn(container, item, slot) or (util.access(item, "prefab") == "barnacle")
     end
 
-    old_test_fn[1] = new_fn
-    container_replica.itemtestfn = new_fn
+    util.get_class(inst.components.container).itemtestfn[1] = new_fn
+    util.get_class(inst.replica).container.itemtestfn = new_fn
 end)
